0bb3d5f75d41957ae9f18a62ef18880b8580d948
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2022-09-27 16:03:49 +0700

    Fix URL 3

--- utils/fetchExperiences.ts
+++ utils/fetchExperiences.ts
@@ -2,7 +2,7 @@ import { Experience } from '../typings';
 
 export const fetchExperiences = async () => {
   const res = await fetch(
-    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/getExperiences`
+    `https://${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperiences`
   );
   const data = await res.json();
   const experiences: Experience[] = data.experiences;
--- utils/fetchSkills.ts
+++ utils/fetchSkills.ts
@@ -2,7 +2,7 @@ import { Skill } from '../typings';
 
 export const fetchSkills = async () => {
   const res = await fetch(
-    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/getSkills`
+    `https://${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`
   );
   const data = await res.json();
   const skills: Skill[] = data.skills;
