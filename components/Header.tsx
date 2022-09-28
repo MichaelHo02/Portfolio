c20dedf14e1246553c2a0671b396c4b53953cecd
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2022-09-28 22:23:09 +0700

    Fix header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,32 +1,35 @@
 import { motion } from "framer-motion"
 import Link from 'next/link'
 import { SocialIcon } from "react-social-icons"
-import { Social } from '../typings'
+import { PageInfo, Social } from '../typings'
 
 type Props = {
-  socials?: Social[]
+  socials?: Social[],
+  pageInfo?: PageInfo
 }
 
-const Header = ({ socials }: Props) => {
+const Header = ({ pageInfo, socials }: Props) => {
   return (
-    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-40 xl:items-center backdrop-blur-sm bg-white/80">
-      <motion.div>
-
-      </motion.div>
-      <motion.div
-        initial={{ x: 500, opacity: 0, scale: 0.5, }}
-        animate={{ x: 0, opacity: 1, scale: 1, }}
-        transition={{ duration: 1 }}
-        className="flex flex-row items-center space-x-2">
-        {socials?.map((social) => (
-          <SocialIcon key={social._id} url={social.url} style={{ width: 40, height: 40 }} />
-        ))}
-        <Link href='#contactMe'>
-          <div className='cursor-pointer heroButton flex items-center'>
-            <p className="uppercase hidden md:inline-flex text-sm text-gray-800">Get In Touch</p>
-          </div>
-        </Link>
-      </motion.div>
+    <header className="sticky top-0 w-screen z-40 xl:items-center backdrop-blur-sm bg-white/80">
+      <div className='flex items-center justify-between max-w-screen-lg mx-auto p-5'>
+        <motion.div>
+          <p className='text-lg font-semibold'>{pageInfo?.name.split(' ')[0]} Portfolio</p>
+        </motion.div>
+        <motion.div
+          initial={{ x: 500, opacity: 0, scale: 0.5, }}
+          animate={{ x: 0, opacity: 1, scale: 1, }}
+          transition={{ duration: 1 }}
+          className="flex flex-row items-center space-x-2">
+          {socials?.map((social) => (
+            <SocialIcon key={social._id} url={social.url} style={{ width: 40, height: 40 }} />
+          ))}
+          <Link href='#contactMe'>
+            <div className='cursor-pointer heroButton flex items-center'>
+              <p className="uppercase hidden md:inline-flex text-sm text-gray-800">Get In Touch</p>
+            </div>
+          </Link>
+        </motion.div>
+      </div>
     </header>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -39,7 +39,7 @@ const Home = ({
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header socials={socials} />
+      <Header pageInfo={pageInfo} socials={socials} />
       <section id="hero">
         <Hero pageInfo={pageInfo} />
       </section>
